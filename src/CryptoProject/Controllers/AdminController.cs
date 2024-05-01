ddfa1f8373501a5d5bbbf2b615c0b23b64719d24
Abdulquddus Nuhu <abdulquddusnuhu@gmail.com>
2024-05-01 08:56:17 +0100

    set status for auto-reverse

--- src/CryptoProject/Controllers/AdminController.cs
+++ src/CryptoProject/Controllers/AdminController.cs
@@ -320,7 +320,7 @@ namespace CryptoProject.Controllers
                 return BadRequest(new BaseResponse() { Message = "Transaction not found", Status = false, Code = 400 });
             }
 
-            if (transaction.Status is TransactionStatus.Reversed)
+            if (transaction.Status is TransactionStatus.AutoReversed)
             {
                 return BadRequest(new BaseResponse() { Message = "Transaction status is already reverted", Status = false, Code = 400 });
             }
@@ -480,7 +480,7 @@ namespace CryptoProject.Controllers
 
             }
 
-            transaction.Status = TransactionStatus.Reversed;
+            transaction.Status = TransactionStatus.AutoReversed;
             transaction.Modified = DateTime.UtcNow;
 
             _dbContext.Transactions.Update(transaction);
---
+++ src/CryptoProject/Migrations/20240501072047_UserCAnTRansact.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserCAnTRansact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanTransact",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanTransact",
+                table: "AspNetUsers");
+        }
+    }
+}
