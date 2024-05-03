11bcb0d834b8de3886bd6fca49414921c5c47964
Abdulquddus Nuhu <abdulquddusnuhu@gmail.com>
2024-05-03 09:29:33 +0100

    fix middleware

--- src/CryptoProject/Program.cs
+++ src/CryptoProject/Program.cs
@@ -233,14 +233,12 @@ try
     var app = builder.Build();
 
     //Configure the HTTP request pipeline.
-    //if (!app.Environment.IsProduction())
-    //{
-    //    app.UseSwagger();
-    //    app.UseSwaggerUI();
-    //}
+    if (!app.Environment.IsProduction())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
 
     //security
     app.UseMiddleware<UserAgentValidationMiddleware>();
